e5f3f229ed27dbc0a04e4bf4a4c790a98f7bf8a0
Damien Hynd <damien.hynd@gmail.com>
2018-09-28 01:34:06 +1000

    Tidied index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
-const home = require('./routes/home');
-const genres = require('./routes/genres');
-const debug = require('debug')('app:startup');
-const config = require('config');
-const Joi = require('joi');
+// Express
 const express = require('express');
 const app = express();
+
+// Packages
+const debug = require('debug')('app:startup');
+const envDebug = require('debug')('app:env');
+const config = require('config');
+const Joi = require('joi');
+
+// Modules
+const home = require('./routes/home');
+const genres = require('./routes/genres');
+const morgan = require('morgan');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -13,9 +20,13 @@ app.set('views', './views');
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/', home);
+app.use(morgan('tiny'));
+
+envDebug('Port: ' + process.env.PORT);
+envDebug('Environment: ' + process.env.NODE_ENV);
+envDebug('Debug namespace: ' + process.env.DEBUG);
 
 if(app.get('env') === 'development') {
-// Configuration
     debug('Application Name: ' + config.get('name'));
     debug('Mail Server: ' + config.get('mail.host'));
     debug('Password: ' + config.get('mail.password'));
