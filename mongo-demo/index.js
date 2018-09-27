5161cb2a0acad5ac8028b68c0a46ea86e11f4e8c
Damien Hynd <damien.hynd@gmail.com>
2018-09-28 01:49:44 +1000

    create schema and create first document

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,27 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDb'))
     .catch(err => console.log('Could not connect', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Damien',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
