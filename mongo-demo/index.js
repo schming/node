0c6b378890616054df70bc44a65b2c7328661c77
Damien Hynd <damien.hynd@gmail.com>
2018-09-28 21:25:15 +1000

    Add get course and delete course functionality

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -16,14 +16,30 @@ const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Angular Course',
-        author: 'Damien',
-        tags: ['angular', 'frontend'],
-        isPublished: true
+        name: 'React Course',
+        author: 'Natalie',
+        tags: ['react', 'frontend'],
+        isPublished: false
     });
 
     const result = await course.save();
     console.log(result);
 }
 
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Damien', isPublished: true })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses);
+}
+
+async function deleteCourse(id) {
+    const result = await Course.findByIdAndDelete(id)
+        .then(console.log('All done', result))
+        .catch((error) => console.log('Something went wrong', error));
+}
+
+// deleteCourse('5badc4ad0babf0210c3f79f2');
 createCourse();
