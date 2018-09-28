49b865c4982e603d7a514df49f470acbd60d67bd
Damien Hynd <damien.hynd@gmail.com>
2018-09-28 21:43:31 +1000

    Added update, delete and get functionality

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -35,11 +35,31 @@ async function getCourses() {
     console.log(courses);
 }
 
-async function deleteCourse(id) {
-    const result = await Course.findByIdAndDelete(id)
-        .then(console.log('All done', result))
-        .catch((error) => console.log('Something went wrong', error));
+async function getCourse(id) {
+    const course = await Course.findById(id).select({ name: 1, author: 1, isPublished: 1 });
+    if(!course) return console.log('That course could not be found in the Db');
+
+    console.log(course);
 }
 
-// deleteCourse('5badc4ad0babf0210c3f79f2');
-createCourse();
+async function deleteCourse(id) {
+    const result = await Course.findByIdAndDelete(id);
+    if(!result) return console.log('An error has occurred, and the entry has not been deleted');
+
+    console.log('All done', result);
+}
+
+async function updateCourse(id) {
+    const result = await Course.findOneAndUpdate({ _id: id }, {
+        author: 'Belter'
+    }, { new: true });
+    if(!result) return console.log('An error has occurred and the entry has not been updated');
+
+    console.log(result);
+}
+
+getCourse('5bacfb7419008504882515e5');
+// getCourses();
+// updateCourse('5bacfb7419008504882515e5');
+// deleteCourse('5bacfbd3f3990320709e16ff');
+// createCourse();
